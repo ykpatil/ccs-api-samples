9e4c519cb8415aa68276665295ea0a4f4dcdba9b
Azad Rai <46309395+AzadRai@users.noreply.github.com>
2019-01-04 14:22:24 +0530

    Update GetRemediationTicketDetailsByID.py
    updated the script

--- python/GetRemediationTicketDetailsByID.py
+++ python/GetRemediationTicketDetailsByID.py
@@ -1,15 +1,29 @@
+# Script to search remediation ticket details based on ticket ID
+# For more details Refer the CCS REST API document at : https://apidocs.symantec.com/home/CCS
+
 import requests
 from requests.packages.urllib3.exceptions import InsecureRequestWarning
+
+# Variable 
+# Replace the <hostname> with CCS application server host name
+# Replace the <port number> with the configured port number for REST API, Default Port Number : 12431
+# Replace the <user name> and <password> with valid CCS user name and password for example: UserName = domain1\\administrator, password = pass@123   
+HostName = '<hostname>'
+PortNumber = '<port number>'
+UserName = '<user name>'
+Password = '<password>'
+
+# Function to generate CCS REST API access token
 def getToken():
-	urlToken = "https://<host name>:<port number>/ccs/api/v1/oauth/tokens"
+	urlToken = "https://" + HostName + ":" + PortNumber + "/ccs/api/v1/oauth/tokens"
 
-	payload = "grant_type=password&username=<user name>&password=<password>"
-	headers = {
-    	'Content-Type': "application/json",
-    }
-
+	payload = "grant_type=password&username=" + UserName + "&password=" + Password +""
+	headers = {'Content-Type': "application/json"}
 	responseToken = requests.request("POST", urlToken, data=payload, headers=headers, verify=False)
-
+	autheticationresult = responseToken.status_code
+	if (autheticationresult!=200) :
+		print("\nToken Generation Failed. Please check if the REST API is enabled and User name and password is correct\n")
+		exit()
 	tokenDict = responseToken.json()
 	token = tokenDict['access_token']
 	refreshToken = tokenDict['refresh_token']
@@ -19,20 +33,18 @@ def getToken():
 	print(refreshToken)
 	return token
 
-url = "https://<host name>:<port number>/ccs/api/v1/RemediationTickets"
+# Remediation ticket details URI
+url = "https://" + HostName + ":" + PortNumber + "/ccs/api/v1/RemediationTickets"
+
+# provide the ticket ID in the query string like-INCA0000001
 
 querystring = {"TicketNumber":"INCA0000001"}
-
 requests.packages.urllib3.disable_warnings(InsecureRequestWarning)
 bearertoken = "Bearer " + getToken()
-
 headers = {
     'Authorization': bearertoken ,
     'Content-Type': "application/json"
     }
-
 response = requests.request("GET", url, headers=headers, params=querystring, verify=False)
-
-
 print(response.text)
 print(response.json)
