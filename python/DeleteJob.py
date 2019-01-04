e3aa6d6c38d7a632840bef58c7e204b4e9bde8a5
Azad Rai <46309395+AzadRai@users.noreply.github.com>
2019-01-04 15:21:52 +0530

    Update DeleteJob.py
    update the script

--- python/DeleteJob.py
+++ python/DeleteJob.py
@@ -1,36 +1,48 @@
+# Script to Delete the custom created job in CCS system 
+# For more details Refer the CCS REST API document at : https://apidocs.symantec.com/home/CCS
+
 import requests
 from requests.packages.urllib3.exceptions import InsecureRequestWarning
+
+# Variable 
+# Replace the <hostname> with CCS application server host name
+# Replace the <port number> with the configured port number for REST API, Default Port Number : 12431
+# Replace the <user name> and <password> with valid CCS user name and password for example: UserName = domain1\\administrator, password = pass@123   
+HostName = '<hostname>'
+PortNumber = '<port number>'
+UserName = '<user name>'
+Password = '<password>'
+
+# Function to generate CCS REST API access token
 def getToken():
-	urlToken = "https://<hostname>:<port number>/ccs/api/v1/oauth/tokens"
+	urlToken = "https://" + HostName + ":" + PortNumber + "/ccs/api/v1/oauth/tokens"
 
-	payload = "grant_type=password&username=<user name>&password=<password>"
-	headers = {
-    	'Content-Type': "application/json",
-    }
-
+	payload = "grant_type=password&username=" + UserName + "&password=" + Password +""
+	headers = {'Content-Type': "application/json"}
 	responseToken = requests.request("POST", urlToken, data=payload, headers=headers, verify=False)
-
+	autheticationresult = responseToken.status_code
+	if (autheticationresult!=200) :
+		print("\nToken Generation Failed. Please check if the REST API is enabled and User name and password is correct\n")
+		exit()
 	tokenDict = responseToken.json()
 	token = tokenDict['access_token']
 	refreshToken = tokenDict['refresh_token']
-
+	print("bearer Token is:\n")
+	print(token)
+	print("\n Refresh Token is:\n")
+	print(refreshToken)
 	return token
-
-url = "https://<hostname>:<port number>/ccs/api/v1/Jobs"
+# CCS job URI
+url = "https://" + HostName + ":" + PortNumber + "/ccs/api/v1/Jobs"
+# provide any valid job GUID like - a282d163-2248-4904-8135-0eaf08be8e55, Customer can use 'Search Job by Name' API to find the job GUID.
 querystring = {"jobid":"<JobID>"}
 payload = "\r"
 requests.packages.urllib3.disable_warnings(InsecureRequestWarning)
-
-
 bearertoken = "Bearer " + getToken()
-print("\n Bearer Token is:\n")
-print(bearertoken + "\n")
-
 headers = {
     'Authorization': bearertoken ,
     'Content-Type': "application/json"
     }
-
 response = requests.request("DELETE", url, data=payload, headers=headers, params=querystring, verify=False)
-
 print(response.text)
+print(response.json)
