3c616f141eab0c5b5af9c4eab7e83da6c16c97ac
Azad Rai <46309395+AzadRai@users.noreply.github.com>
2019-01-07 16:09:24 +0530

    Update SearchJobByID.py
    Updated the script

--- python/SearchJobByID.py
+++ python/SearchJobByID.py
@@ -1,34 +1,44 @@
+#Script to search job details by providing job GUID in the request.
+#For more Details, Refer to CCS REST API document at: https://apidocs.symantec.com/home/CCS
+
+import requests
 import requests
 from requests.packages.urllib3.exceptions import InsecureRequestWarning
+
+# Declare Variables 
+# Replace the <hostname> with CCS application server host name
+# Replace the <port number> with the configured port number for REST API, Default Port Number : 12431
+# Replace the <user name> and <password> with valid CCS user name and password for example: UserName = domain1\\administrator, password = pass@123   
+HostName = '<hostname>'
+PortNumber = '<port number>'
+UserName = '<user name>'
+Password = '<password>'
+
+# Function to generate CCS REST API access token
 def getToken():
-	urlToken = "https://<hostname>:<port number>/ccs/api/v1/oauth/tokens"
-
-	payload = "grant_type=password&username=<user name>&password=<password>"
-	headers = {
-    	'Content-Type': "application/json",
-    }
-
+	urlToken = "https://" + HostName + ":" + PortNumber + "/ccs/api/v1/oauth/tokens"
+	payload = "grant_type=password&username=" + UserName + "&password=" + Password +""
+	headers = {'Content-Type': "application/json"}
 	responseToken = requests.request("POST", urlToken, data=payload, headers=headers, verify=False)
-
+	autheticationresult = responseToken.status_code
+	if (autheticationresult!=200) :
+	    print("\nToken Generation Failed. Please check if the REST API is enabled and User name and password is correct\n")
+	    exit()
 	tokenDict = responseToken.json()
 	token = tokenDict['access_token']
-	refreshToken = tokenDict['refresh_token']
-
+	refreshToken = tokenDict['refresh_token']                
 	return token
 
-url = "https://<hostname>:<port number>/ccs/api/v1/Jobs/<JobID>"
+#Search Job by ID API endpoint URL. Provide the job GUID in the request for searching job details for specific job.
+url = "https://" + HostName + ":" + PortNumber + "/ccs/api/v1/Jobs/a282d163-2248-4904-8135-0eaf08be8e55"
 
 requests.packages.urllib3.disable_warnings(InsecureRequestWarning)
-
 bearertoken = "Bearer " + getToken()
-#print("\n Bearer Token is:\n")
-print(bearertoken + "\n")
 
 headers = {
     'Authorization': bearertoken,
-    'Content-Type': "application/json"
-    }
-
+    'Content-Type': "application/json" 
+    }                      
 response = requests.request("GET", url, headers=headers, verify=False)
-
 print(response.text)
+print(response.json)
