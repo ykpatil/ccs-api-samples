991625baaf9f0fd13712e864b01e649331bd05f4
Azad Rai <46309395+AzadRai@users.noreply.github.com>
2019-01-08 14:30:22 +0530

    Update SearchAssets.py
    updated the script

--- python/SearchAssets.py
+++ python/SearchAssets.py
@@ -1,22 +1,40 @@
+# Script to Search the assets in the CCS System
+# For more details Refer the CCS REST API document at : https://apidocs.symantec.com/home/CCS
+
 import requests
 from requests.packages.urllib3.exceptions import InsecureRequestWarning
 
-# Get the Access Token and use it for the further API calls.
+# Variable 
+# Replace the <hostname> with CCS application server host name
+# Replace the <port number> with the configured port number for REST API, Default Port Number : 12431
+# Replace the <user name> and <password> with valid CCS user name and password for example: UserName = domain1\\administrator, password = pass@123   
+HostName = '<hostname>'
+PortNumber = '<port number>'
+UserName = '<user name>'
+Password = '<password>'
+
+
+# Function to generate CCS REST API access token
 def getToken():
-	urlToken = "https://<hostname>:<port number>/ccs/api/v1/oauth/tokens"
+	urlToken = "https://" + HostName + ":" + PortNumber + "/ccs/api/v1/oauth/tokens"
 
-	payload = "grant_type=password&username=<user name>&password=<password>"
-	headers = {
-    	'Content-Type': "application/json",
-    }
-
+	payload = "grant_type=password&username=" + UserName + "&password=" + Password +""
+	headers = {'Content-Type': "application/json"}
 	responseToken = requests.request("POST", urlToken, data=payload, headers=headers, verify=False)
+	autheticationresult = responseToken.status_code
+	if (autheticationresult!=200) :
+		print("\nToken Generation Failed. Please check if the REST API is enabled and User name and password is correct\n")
+		exit()
 	tokenDict = responseToken.json()
 	token = tokenDict['access_token']
 	refreshToken = tokenDict['refresh_token']
+	print("bearer Token is:\n")
+	print(token)
+	print("\n Refresh Token is:\n")
+	print(refreshToken)
 	return token
 
-url = "https://<hostname>:<port number>/ccs/api/v1/Assets"
+url = "https://" + HostName + ":" + PortNumber + "/ccs/api/v1/Assets"
 
 ## Simple Search
 #querystring = {"attributes":"displayname Contains 10.211.64"}
@@ -26,12 +44,9 @@ querystring = {"attributes":"(symc-csm-AssetSystem-Asset-unix-Machine-OSSystem C
 "searchsubtree":"false","page":"0","pagesize":"1"}
 requests.packages.urllib3.disable_warnings(InsecureRequestWarning)
 bearertoken = "Bearer " + getToken()
-
 headers = {
     'Authorization': bearertoken
     }
-
 response = requests.request("GET", url, headers=headers, params=querystring, verify=False)
-
 assetsData = response.json()
 print(response.text)
